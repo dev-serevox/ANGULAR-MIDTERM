2d14aea89aeabf305364a2ebae56592672df09f8
iamnoieee <89822798+iamnoieee@users.noreply.github.com>
2023-03-26 22:33:31 +0800

    2 files modified

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Employee } from '../employee-data';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
